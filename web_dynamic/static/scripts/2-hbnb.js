b19d2504151a47d675f3c5f691f804f40859f781
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-02-03 11:13:21 +0100

    Api status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,39 @@
+$(window).on( "load", function () {
+    // API status check
+    $.get("http://0.0.0.0:5001/api/v1/status/", function (data, status) {
+        if (data["status"] == "OK") {
+            $("#api_status").attr("class", "available")
+        } else {
+            $("#api_status").removeAttr("class")
+        }
+    })
+    // Amenities selection
+    let amenity_ids = [];
+    let amenity_names = []
+    $("input:checkbox").on("click", function () {
+        let id = $(this).attr("data-id")
+        let name = $(this).attr("data-name")
+        if (!amenity_ids.includes(id)) {
+            amenity_ids.push(id)
+            amenity_names.push(name)
+        } else {
+            let index = amenity_ids.indexOf(id)
+            amenity_ids.splice(index, 1)
+            amenity_names.splice(index, 1)
+        }
+
+        let h4_text; 
+        if (amenity_names.length === 0) {
+            h4_text = "&nbsp;"
+        } else {
+            h4_text = ""
+            for (let i = 0; i < amenity_names.length; i++) {
+                h4_text += amenity_names[i]
+                if (i !== (amenity_names.length - 1)) {
+                    h4_text += ", "
+                } 
+            }
+        }
+        $(".amenities h4").html(h4_text)
+    })
+});
